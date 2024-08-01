1f58cb62443ba12061bfef67b0b248b8a52b2f41
jeremias210421 <jeremias210421@gmail.com>
2024-08-01 12:58:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('.nav-menu');
+    const overlay = document.querySelector('.overlay');
 
     menuToggle.addEventListener('click', () => {
         navMenu.classList.toggle('active');
+        overlay.classList.toggle('active');
+        document.body.style.overflow = navMenu.classList.contains('active') ? 'hidden' : '';
+    });
+
+    overlay.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
     });
 });
